5d7a23fda40f52f83972607416b7088c142dabc0
hufan30 <hufan30@163.com>
2021-05-06 16:12:10 +0800

    Update GPAfterReturningAdviceInterceptor.java

--- gupaoedu-vip-spring-2.0/src/main/java/com/gupaoedu/vip/spring/formework/aop/aspect/GPAfterReturningAdviceInterceptor.java
+++ gupaoedu-vip-spring-2.0/src/main/java/com/gupaoedu/vip/spring/formework/aop/aspect/GPAfterReturningAdviceInterceptor.java
@@ -18,6 +18,12 @@ public class GPAfterReturningAdviceInterceptor extends GPAbstractAspectAdvice im
 
     @Override
     public Object invoke(GPMethodInvocation mi) throws Throwable {
+        /**
+         * 居然是在这里用了链式的思维，之前一直纠结为什么链式调用过程中，真正的方法执行能在before和after中间执行
+         * 猜想一是把本来的方法也放在了链路中，这样根据链路顺序执行的时候就按顺序执行
+         * 但是实际好像不是这样，这里看有一个函数的嵌套思路；
+         * 就是在after的调用过程中，再来一次proceed，这样在执行after方法之前，就先执行了真正的方法；
+         */
         Object retVal = mi.proceed();
         this.joinPoint = mi;
         this.afterReturning(retVal,mi.getMethod(),mi.getArguments(),mi.getThis());
